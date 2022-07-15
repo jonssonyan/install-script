211d55788207efd818b46abc144c3e2a533ef7bf
jonssonyan <yz808@outlook.com>
2022-07-15 09:43:02 +0800

    init

--- k8s-install-centos7.sh
+++ k8s-install-centos7.sh
@@ -150,6 +150,7 @@ install_prepare() {
   cat >/etc/modules-load.d/k8s.conf <<EOF
 br_netfilter
 EOF
+
   cat >/etc/sysctl.d/k8s.conf <<EOF
 net.bridge.bridge-nf-call-ip6tables = 1
 net.bridge.bridge-nf-call-iptables = 1
