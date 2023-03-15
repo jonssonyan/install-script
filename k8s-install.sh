5e34a7e1d5f79ffa91e48e3c8083559d70bf7bdd
jonssonyan <yz808@outlook.com>
2023-03-15 12:20:03 +0800

    fix: script

--- k8s-install.sh
+++ k8s-install.sh
@@ -443,7 +443,8 @@ setup_k8s() {
   cat >/etc/sysconfig/kubelet <<EOF
     KUBELET_EXTRA_ARGS="--cgroup-driver=systemd"
 EOF
-  crictl config runtime-endpoint unix:///run/containerd/containerd.sock
+  crictl config runtime-endpoint unix:///var/run/containerd/containerd.sock
+  crictl config image-endpoint unix:///var/run/containerd/containerd.sock
 }
 
 # 安装k8s
