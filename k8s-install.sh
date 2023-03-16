1a8ddf506a20d6c59002e1b5936ba15a904d06d7
jonssonyan <yz808@outlook.com>
2023-03-16 19:36:48 +0800

    fix: script

--- k8s-install.sh
+++ k8s-install.sh
@@ -36,7 +36,7 @@ init_var() {
 
   # Docker
   docker_version="20.10.23"
-  docker_mirror='"https://dockerhub.azk8s.cn","https://hub-mirror.c.163.com","https://docker.mirrors.ustc.edu.cn","https://registry.docker-cn.com"'
+  docker_mirror='"https://dockerhub.azk8s.cn","https://docker.mirrors.ustc.edu.cn","https://hub-mirror.c.163.com","https://registry.docker-cn.com"'
 }
 
 echo_content() {
