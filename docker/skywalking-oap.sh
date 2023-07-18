e6d3325bd306190d419fd891e31590994fa6050b
jonssonyan <yz808@outlook.com>
2023-07-18 16:53:21 +0800

    fix: sw

--- docker/skywalking-oap.sh
+++ docker/skywalking-oap.sh
@@ -10,10 +10,6 @@ init_var() {
   sw_oap_http=12800
   sw_oap_grpc=11800
 
-  sw_ui_ip="js-skywalking-ui"
-  sw_ui_port=8080
-  sw_oap_url="http://127.0.0.1:12800"
-
   es_url="http://127.0.0.1:9200"
   es_username="elastic"
   es_password="elastic"
@@ -118,31 +114,6 @@ install_skywalking() {
     fi
   else
     echo_content skyBlue "---> 你已经安装了SkyWalking OAP"
-  fi
-
-  if [[ -z $(docker ps -q -f "name=^${sw_ui_ip}$") ]]; then
-    echo_content green "---> 安装SkyWalking UI"
-
-    read -r -p "请输入SkyWalking UI的端口(默认:8080): " sw_ui_port
-    [[ -z "${sw_ui_port}" ]] && sw_ui_port=8080
-    read -r -p "请输入SkyWalking OAP的URL(默认:http://127.0.0.1:12800): " sw_oap_url
-    [[ -z "${sw_oap_url}" ]] && sw_oap_url="http://127.0.0.1:12800"
-
-    docker run -d --name ${sw_ui_ip} --restart always \
-      --network=host \
-      -e TZ=Asia/Shanghai \
-      -e SW_SERVER_PORT=${sw_ui_port} \
-      -e SW_OAP_ADDRESS="${sw_oap_url}" \
-      apache/skywalking-ui:9.5.0
-
-    if [[ -n $(docker ps -q -f "name=^${sw_ui_ip}$" -f "status=running") ]]; then
-      echo_content skyBlue "---> SkyWalking UI安装完成"
-    else
-      echo_content red "---> SkyWalking UI安装失败或运行异常,请尝试修复或卸载重装"
-      exit 1
-    fi
-  else
-    echo_content skyBlue "---> 你已经安装了SkyWalking UI"
   fi
 }
 
