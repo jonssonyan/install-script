21bfb78cf35f8cae4ca881a117479c35574f66af
jonssonyan <yz808@outlook.com>
2024-07-15 10:58:22 +0800

    fix: format

--- docker/buildx.sh
+++ docker/buildx.sh
@@ -44,7 +44,7 @@ install_docker() {
 install_buildx() {
   docker buildx inspect --bootstrap | grep -q "mybuilder"
   if [[ "$?" != "0" ]]; then
-    echo_content green "---> 安装buildx交叉编译"
+    echo_content green "---> 安装 buildx 交叉编译"
 
     if [[ -d "${DOCKER_CONFIG_PATH}" && -f "${docker_config}" ]]; then
       if ! grep -q "experimental" "${docker_config}"; then
@@ -64,13 +64,13 @@ EOF
       docker run --privileged --rm tonistiigi/binfmt --install all
 
     if docker buildx inspect --bootstrap | grep -q "mybuilder"; then
-      echo_content skyBlue "---> buildx交叉编译安装完成"
+      echo_content skyBlue "---> buildx 交叉编译安装完成"
     else
-      echo_content red "---> buildx交叉编译安装失败"
+      echo_content red "---> buildx 交叉编译安装失败"
       exit 1
     fi
   else
-    echo_content skyBlue "---> 你已经安装了buildx交叉编译"
+    echo_content skyBlue "---> 你已经安装了 buildx 交叉编译"
   fi
 }
 
