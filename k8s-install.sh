27bcde8b7a1a9cf6d1c8c297540e5e11e27d0307
jonssonyan <yz808@outlook.com>
2024-11-13 11:35:23 +0800

    fix: mirror

--- k8s-install.sh
+++ k8s-install.sh
@@ -708,7 +708,7 @@ k8s_install() {
     [[ -z "${host_name}" ]] && host_name="k8s-master"
     set_hostname ${host_name}
 
-    while read -r -p "请输入 K8s 版本(1.23.17,1.24-1.31 默认:1.30): " k8s_version; do
+    while read -r -p "请输入 K8s 版本(1.23.17,1.24-1.31 默认:1.31): " k8s_version; do
       [[ -z "${k8s_version}" ]] && k8s_version="1.31"
       if echo "${k8s_versions}" | grep -w -q "${k8s_version}"; then
         break
