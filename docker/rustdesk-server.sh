27aefc50ec75b95fa99b6f78045bc894a4e257fb
jonssonyan <yz808@outlook.com>
2025-03-04 14:41:18 +0800

    feat: rustdesk server

--- docker/rustdesk-server.sh
+++ docker/rustdesk-server.sh
@@ -7,7 +7,10 @@ init_var() {
 
   # RustDesk Server
   RUSTDESK_SERVER="/jydata/rustdesk-server/"
-  rustdesk_server_ip="jy-rustdesk-server"
+  # bbr
+  rustdesk_server_bbr="jy-rustdesk-server-bbr"
+  # hbbs
+  rustdesk_server_hbbs="jy-rustdesk-server-hbbs"
 }
 
 echo_content() {
@@ -45,11 +48,39 @@ install_docker() {
   bash <(curl -fsSL https://github.com/jonssonyan/install-script/raw/main/docker/install.sh)
 }
 
-install_rustdesk_server() {
-  if [[ -z $(docker ps -q -f "name=^${rustdesk_server_ip}$") ]]; then
-    echo_content green "---> 安装 RustDesk Server"
+install_rustdesk_bbr() {
+  if [[ -z $(docker ps -q -f "name=^${rustdesk_server_bbr}$") ]]; then
+    echo_content green "---> 安装 RustDesk Server bbr"
+
+    docker run -d --name hbbr --restart always \
+      --network=host \
+      -e TZ=Asia/Shanghai \
+      -v ${RUSTDESK_SERVER}data/:/root \
+      rustdesk/rustdesk-server-pro
+
   else
-    echo_content skyBlue "---> 你已经安装了 RustDesk Server"
+    echo_content skyBlue "---> 你已经安装了 RustDesk Server bbr"
+  fi
+
+  if [[ -z $(docker ps -q -f "name=^${rustdesk_server_bbr}$") ]]; then
+    install_rustdesk_hbbs
+  fi
+
+}
+
+install_rustdesk_hbbs() {
+  if [[ -z $(docker ps -q -f "name=^${rustdesk_server_hbbs}$") ]]; then
+    echo_content green "---> 安装 RustDesk Server hbbs"
+
+    docker run -d --name hbbs --restart always \
+      --network=host \
+      -e TZ=Asia/Shanghai \
+      --depends-on hbbr \
+      -v ${RUSTDESK_SERVER}data/:/root \
+      rustdesk/rustdesk-server-pro
+
+  else
+    echo_content skyBlue "---> 你已经安装了 RustDesk Server hbbs"
   fi
 }
 
@@ -58,4 +89,4 @@ init_var
 mkdir_tools
 clear
 install_docker
-install_rustdesk_server
+install_rustdesk_bbr
